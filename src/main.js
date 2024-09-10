a52bd0529411f5540fe4117b6ea019085393c98e
Khrystyna Kozakevych <margaret.gray.777@gmail.com>
2024-09-10 18:14:11 +0200

    update js, html and css

--- src/main.js
+++ src/main.js
@@ -1,13 +1,2 @@
-// show more catalog btn ------------------------
-const showMore = document.querySelector('.showmore-catalog-btn');
-let items = 4;
-const catalogLength = document.querySelectorAll('.catalog-item').length;
-showMore.addEventListener('click', () => {
-  items += 1;
-  const array = Array.from(document.querySelector('.catalog-list').children);
-  const visItems = array.slice(0, items);
-  visItems.forEach(el => el.classList.add('is-visible'));
-  if (visItems.length === catalogLength) {
-    showMore.style.display = 'none';
-  }
-});
+import "./js/catalog.js";
+import "./js/menu.js";
